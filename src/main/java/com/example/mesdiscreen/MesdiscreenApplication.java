7e64c1e3da67ea95f960a67b67c398e2a812162f
slingshot-dev <slingshot@outlook.fr>
2021-03-01 08:44:56 +0100

    Structure Projet

---
+++ src/main/java/com/example/mesdiscreen/MesdiscreenApplication.java
@@ -0,0 +1,13 @@
+package com.example.mesdiscreen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MesdiscreenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MesdiscreenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mesdiscreen/MesdiscreenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mesdiscreen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MesdiscreenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
